5d73246403449ef744d43c4380b22c70b74e077e
向辉 <xianghui@corp.netease.com>
2023-05-17 15:58:55 +0800

    update init

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ rm -rf chinese-llama-plus-lora-7b
 rm -rf Chinese-LLaMA-Alpaca
 git clone https://huggingface.co/decapoda-research/llama-7b-hf
 git clone https://huggingface.co/ziqingyang/chinese-llama-plus-lora-7b
-git clone https://github.com/xianghui123/Chinese-LLaMA-Alpaca
+git clone https://github.com/coddderX/Chinese-LLaMA-Alpaca
 cd Chinese-LLaMA-Alpaca
 python -m pip install -r requirements.txt
