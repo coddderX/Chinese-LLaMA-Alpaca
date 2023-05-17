2e84119e996b9ede5a85c768ccf6b0a4cfd6171a
向辉 <xianghui@corp.netease.com>
2023-05-17 16:19:45 +0800

    add merge .sh

---
+++ scripts/merge_chinese_llama.sh
@@ -0,0 +1,5 @@
+python merge_llama_with_chinese_lora.py \
+    --base_model ../../llama-7b-hf \
+    --lora_model ../../chinese-llama-plus-lora-7b \
+    --output_type huggingface \
+    --output_dir ../../chinese-llama-7b
