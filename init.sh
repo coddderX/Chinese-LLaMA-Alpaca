98037251468b0622330d080b505d6ea574d10328
向辉 <xianghui@corp.netease.com>
2023-05-17 15:55:07 +0800

    update init

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 cd /notebook
 apt-get install git-lfs
 rm -rf llama-7b-hf
-rm -rf Chinese-Vicuna
+rm -rf Chinese-LLaMA-Alpaca
 git clone https://huggingface.co/decapoda-research/llama-7b-hf
 git clone https://huggingface.co/ziqingyang/chinese-llama-plus-lora-7b
-git clone https://github.com/xianghui123/Chinese-Vicuna
-cd Chinese-Vicuna
+git clone https://github.com/xianghui123/Chinese-LLaMA-Alpaca
+cd Chinese-LLaMA-Alpaca
 python -m pip install -r requirements.txt
