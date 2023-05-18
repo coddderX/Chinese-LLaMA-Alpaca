fbab491c4c385e309877726ff2996afe1603155c
向辉 <xianghui@corp.netease.com>
2023-05-18 16:01:22 +0800

    fix

--- scripts/merge_my_lora.sh
+++ scripts/merge_my_lora.sh
@@ -6,6 +6,6 @@ mv /notebook/my-llama-7b-lora/20230518/pytorch_model.bin /notebook/my-llama-7b-l
 
 python merge_llama_with_chinese_lora.py \
     --base_model /notebook/chinese-llama-7b \
-    --lora_model ./notebook/my-llama-7b-lora/20230518 \
+    --lora_model /notebook/my-llama-7b-lora/20230518 \
     --output_type huggingface \
     --output_dir /notebook/my-llama-7b/20230518
