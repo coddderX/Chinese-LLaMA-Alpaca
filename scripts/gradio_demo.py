e28d87b6c9878050e11076c5745860cb3a609d49
coddderX <2446777141@qq.com>
2023-05-25 17:49:21 +0800

    Update gradio_demo.py

--- scripts/gradio_demo.py
+++ scripts/gradio_demo.py
@@ -145,10 +145,10 @@ def predict(
     return chatbot, history
 
 with gr.Blocks() as demo:
-    gr.HTML("""<h1 align="center">Chinese LLaMA & Alpaca LLM</h1>""")
+    # gr.HTML("""<h1 align="center">Chinese LLaMA & Alpaca LLM</h1>""")
     current_file_path = os.path.abspath(os.path.dirname(__file__))
-    gr.Image(f'{current_file_path}/../pics/banner.png', label = 'Chinese LLaMA & Alpaca LLM')
-    gr.Markdown("> 为了促进大模型在中文NLP社区的开放研究，本项目开源了中文LLaMA模型和指令精调的Alpaca大模型。这些模型在原版LLaMA的基础上扩充了中文词表并使用了中文数据进行二次预训练，进一步提升了中文基础语义理解能力。同时，中文Alpaca模型进一步使用了中文指令数据进行精调，显著提升了模型对指令的理解和执行能力")
+    # gr.Image(f'{current_file_path}/../pics/banner.png', label = 'Chinese LLaMA & Alpaca LLM')
+    # gr.Markdown("> 为了促进大模型在中文NLP社区的开放研究，本项目开源了中文LLaMA模型和指令精调的Alpaca大模型。这些模型在原版LLaMA的基础上扩充了中文词表并使用了中文数据进行二次预训练，进一步提升了中文基础语义理解能力。同时，中文Alpaca模型进一步使用了中文指令数据进行精调，显著提升了模型对指令的理解和执行能力")
     chatbot = gr.Chatbot()
     with gr.Row():
         with gr.Column(scale=4):
@@ -160,7 +160,7 @@ with gr.Blocks() as demo:
         with gr.Column(scale=1):
             emptyBtn = gr.Button("Clear History")
             max_length = gr.Slider(
-                0, 4096, value=128, step=1.0, label="Maximum length", interactive=True)
+                0, 4096, value=800, step=1.0, label="Maximum length", interactive=True)
             top_p = gr.Slider(0, 1, value=0.8, step=0.01,
                               label="Top P", interactive=True)
             temperature = gr.Slider(
